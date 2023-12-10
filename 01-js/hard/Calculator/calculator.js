14d2103fd2fd298b1794a6256627a7524d05603f
SaiKapilKumar <kaps1612003@gmail.com>
2023-12-11 00:00:56 +0530

    minor changes

--- 01-js/hard/Calculator/calculator.js
+++ 01-js/hard/Calculator/calculator.js
@@ -1,56 +1,4 @@
-/*
-  Implement a class `Calculator` having below methods
-    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
-    - add: takes a number and adds it to the result
-    - subtract: takes a number and subtracts it from the result
-    - multiply: takes a number and multiply it to the result
-    - divide: takes a number and divide it to the result
-    - clear: makes the `result` variable to 0
-    - getResult: returns the value of `result` variable
-    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
-      Points to Note: 
-        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
-        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
-
-  Once you've implemented the logic, test your code by running
-*/
-
-class Calculator {
-  constructor() {
-    this.result = 0;
-  }
-
-  set(num){
-    this.result = num;
-  }
-
-  add(num) {
-    this.result += num;
-  }
-
-  sub(num){
-    this.result -= num;
-  }
-
-  mul(num){
-    this.result *= num;
-  }
-
-  div(num){
-    if(num === 0) {
-      throw new Error('Cannot divide by zero');
-    }this.result /= num;
-  }
-
-  clear(){
-    this.result = 0;
-  }
-
-  getResult(){
-    return this.result;
-  }
-}
+// Status: Complete
 
 let inputBox = document.getElementById('input');
 let outputBox = document.getElementById('output');
@@ -78,8 +26,6 @@ let equalsbtn = document.getElementById('equals');
 
 let zerobtn = document.getElementById('zero');
 let decimalbtn = document.getElementById('decimal');
-
-let calculator = new Calculator();
 
 ACbtn.addEventListener('click', () => {
   inputBox.value = '';
