8e97f2ad861f51551bb291985490b4c258327f8d
SaiKapilKumar <kaps1612003@gmail.com>
2023-12-10 23:59:18 +0530

    Add calculator functionality to index.html and calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,40 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  set(num){
+    this.result = num;
+  }
+
+  add(num) {
+    this.result += num;
+  }
+
+  sub(num){
+    this.result -= num;
+  }
+
+  mul(num){
+    this.result *= num;
+  }
+
+  div(num){
+    if(num === 0) {
+      throw new Error('Cannot divide by zero');
+    }this.result /= num;
+  }
+
+  clear(){
+    this.result = 0;
+  }
+
+  getResult(){
+    return this.result;
+  }
+}
 
 module.exports = Calculator;
